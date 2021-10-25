ab3f62de835ee349d08075d5057447aa975dee2a
Tharsikan <thar.navaratnam@gmail.com>
2021-10-25 17:24:05 +0000

    implemented assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  const message = actual === expected ?
+    `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `❌❌❌ Assertion Failed: ${actual} !== ${expected}`;
+  console.log(message);
+};
+
+// Test Code
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
