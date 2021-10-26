56033cc43e7f7688729c0bcd639934bf2744b5f1
Tharsikan <thar.navaratnam@gmail.com>
2021-10-26 14:06:44 +0000

    Refactored code

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,13 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   const message = actual === expected ?
 
     `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `❌❌❌ Assertion Failed: ${actual} !== ${expected}`;
   console.log(message);
 };
 
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) return false
-  return arr1.every((elem, i) => elem === arr2[i])
-}
+const eqArrays = function(arr1, arr2) {
+  return arr1.length !== arr2.length ? false : arr1.every((elem, i) => elem === arr2[i]);
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
