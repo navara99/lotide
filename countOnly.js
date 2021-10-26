3ecd0c4d445ffba0865f980529f05fc07b5e7ab1
Tharsikan <thar.navaratnam@gmail.com>
2021-10-26 23:02:55 +0000

    add if condition within loop

--- countOnly.js
+++ countOnly.js
@@ -7,7 +7,7 @@ const countOnly = function (allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    results[item] = results[item] + 1 || 1
+    if (itemsToCount[item]) results[item] = results[item] + 1 || 1
   }
 
   return results;
