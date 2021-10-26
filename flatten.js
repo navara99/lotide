df2229450c71ad46c550c73a9263730b3145f8e6
Tharsikan <thar.navaratnam@gmail.com>
2021-10-26 17:48:09 +0000

    add comments with explanation

--- flatten.js
+++ flatten.js
@@ -34,6 +34,17 @@ const flatten = function(arr) {
 };
 
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+
+// Explanation
+
+// First flatten call adds flatten([1, 2, [3, 4], 5, [6]]) to the call stack
+// Second call is within the loop when the array element is another array
+// This call adds the flatten to the top of the call stack with new params => flatten([[3, 4])
+// 3 and 4 get pushed to the flattenedArr and the flattenedArr gets returned .
+// flatten([[3, 4]) pops out of the call stack
+// loop through the returned array and add items into the flattenedArr in the first call in the stack (flatten([1, 2, [3, 4], 5, [6]]))
+// Repeat until loop in initial function call ends, then return the total array
+
 assertArraysEqual(flatten([2, [3, 4, [5, 8]], 5, ["a"]]), [2, 3, 4, 5, 8, 5, "a"]);
 assertArraysEqual(flatten([]), []);
 assertArraysEqual(flatten([3]), [3]);
