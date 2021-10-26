52005476ad186e0ad609047a759c59c21247327e
Tharsikan <thar.navaratnam@gmail.com>
2021-10-26 14:04:16 +0000

    Refactored code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,8 +3,12 @@ const logMessage = (isEqual, arr1, arr2) => {
   console.log(message);
 };
 
+const eqArrays = function(arr1, arr2) {
+  return arr1.length !== arr2.length ? false : arr1.every((elem, i) => elem === arr2[i]);
+};
+
 const assertArraysEqual = function(arr1, arr2) {
-  const isEqual = arr1.length !== arr2.length ? false : arr1.every((elem, i) => elem === arr2[i]);
+  const isEqual = eqArrays(arr1, arr2);
   logMessage(isEqual, arr1, arr2);
 };
 
@@ -12,5 +16,5 @@ assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual([],[]);
-assertArraysEqual([1,2],[]);
+assertArraysEqual([], []);
+assertArraysEqual([1, 2], []);
