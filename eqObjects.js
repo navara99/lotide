e07607d49a7585071b5e1f3745f4850f9424246b
Tharsikan <thar.navaratnam@gmail.com>
2021-10-27 00:56:15 +0000

    refactor code

--- eqObjects.js
+++ eqObjects.js
@@ -1,14 +1,24 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   const message = actual === expected ?
     `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `❌❌❌ Assertion Failed: ${actual} !== ${expected}`;
   console.log(message);
 };
 
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   return arr1.length !== arr2.length ? false : arr1.every((elem, i) => elem === arr2[i]);
 };
 
-const eqObjects = function (object1, object2) {
+const keyValuesEqual = (object1, object2, obj1Keys) => {
+  return obj1Keys.every((key) => {
+    if (Array.isArray(object1[key])) {
+      return eqArrays(object1[key], object2[key]);
+    } else {
+      return object1[key] === object2[key];
+    }
+  });
+};
+
+const eqObjects = function(object1, object2) {
   const obj1Keys = Object.keys(object1);
   const obj2Keys = Object.keys(object2);
   const obj1Length = obj1Keys.length;
@@ -16,25 +26,19 @@ const eqObjects = function (object1, object2) {
 
   if (obj1Length !== obj2Length) return false;
 
-  return obj1Keys.every((key) => {
-    if (Array.isArray(object1[key])) {
-      return eqArrays(object1[key], object2[key])
-    } else {
-      return object1[key] === object2[key]
-    }
-  });
-}
+  return keyValuesEqual(object1, object2, obj1Keys);
+};
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true)
+assertEqual(eqObjects(ab, ba), true);
 
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false)
+assertEqual(eqObjects(ab, abc), false);
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true)
+assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false)
+assertEqual(eqObjects(cd, cd2), false);
