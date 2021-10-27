0c47e9738fa1ab378cb7b3543b9c6adaea38f7ec
Tharsikan <thar.navaratnam@gmail.com>
2021-10-27 00:49:37 +0000

    working solution for arrays

--- eqObjects.js
+++ eqObjects.js
@@ -4,6 +4,10 @@ const assertEqual = function (actual, expected) {
   console.log(message);
 };
 
+const eqArrays = function (arr1, arr2) {
+  return arr1.length !== arr2.length ? false : arr1.every((elem, i) => elem === arr2[i]);
+};
+
 const eqObjects = function (object1, object2) {
   const obj1Keys = Object.keys(object1);
   const obj2Keys = Object.keys(object2);
@@ -12,7 +16,13 @@ const eqObjects = function (object1, object2) {
 
   if (obj1Length !== obj2Length) return false;
 
-  return obj1Keys.every((key) => object1[key] === object2[key]);
+  return obj1Keys.every((key) => {
+    if (Array.isArray(object1[key])) {
+      return eqArrays(object1[key], object2[key])
+    } else {
+      return object1[key] === object2[key]
+    }
+  });
 }
 
 const ab = { a: "1", b: "2" };
@@ -21,3 +31,10 @@ assertEqual(eqObjects(ab, ba), true)
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false)
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true)
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false)
