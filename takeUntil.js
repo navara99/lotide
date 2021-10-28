5ceb139dfc084d42582f7e23528d4199e13449fc
Tharsikan <thar.navaratnam@gmail.com>
2021-10-28 00:59:03 +0000

    working solution

--- takeUntil.js
+++ takeUntil.js
@@ -15,8 +15,15 @@ const assertArraysEqual = function (arr1, arr2) {
 };
 
 const takeUntil = function (array, callback) {
-  
-
+  const takenArr = [];
+  for (const item of array) {
+    if (!callback(item)) {
+      takenArr.push(item);
+    } else {
+      return takenArr
+    }
+  }
+  return takenArr
 }
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
@@ -26,4 +33,8 @@ assertArraysEqual(results1, [1, 2, 5, 7, 2])
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood'])
+
+const data3 = ["to", "building", "lasagna", "heart", "not", "give"];
+const results3 = takeUntil(data3, x => x.length === 3);
+assertArraysEqual(results3, ["to", "building", "lasagna", "heart"])
 
