3e1e7ce9eed24faa5ae5644c7f945808aedf8bd3
Tharsikan <thar.navaratnam@gmail.com>
2021-10-28 00:42:27 +0000

    initial commit

---
+++ takeUntil.js
@@ -0,0 +1,21 @@
+const logMessage = (isEqual, arr1, arr2) => {
+  const message = isEqual ? `✅✅✅ Assertion Passed: ${arr1} === ${arr2}` : `❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`;
+  console.log(message);
+};
+
+const eqArrays = function(arr1, arr2) {
+  return arr1.length !== arr2.length ? false : arr1.every((elem, i) => elem === arr2[i]);
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const isEqual = eqArrays(arr1, arr2);
+  logMessage(isEqual, arr1, arr2);
+};
+
+
+
+const takeUntil = function(array, callback) {
+  
+
+}
+
