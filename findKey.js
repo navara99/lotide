4375864a7ab89d53a8490529f93bfe33e485aba9
Tharsikan <thar.navaratnam@gmail.com>
2021-10-28 01:17:58 +0000

    linting

--- findKey.js
+++ findKey.js
@@ -1,13 +1,13 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   const message = actual === expected ?
     `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `❌❌❌ Assertion Failed: ${actual} !== ${expected}`;
   console.log(message);
 };
 
-const findKey = function (data, callback) {
+const findKey = function(data, callback) {
   for (const key in data) {
-    const result = callback(data[key])
-    if (result) return key
+    const result = callback(data[key]);
+    if (result) return key;
   }
 };
 
@@ -18,16 +18,16 @@ const test1Data = {
   "elBulli": { stars: 3 },
   "Ora": { stars: 2 },
   "Akelarre": { stars: 3 }
-}
+};
 
-const results1 = findKey(test1Data, x => x.stars === 2)
-assertEqual(results1, "noma")
+const results1 = findKey(test1Data, x => x.stars === 2);
+assertEqual(results1, "noma");
 
-const results2 = findKey(test1Data, x => x.stars === 3)
-assertEqual(results2, "Akaleri")
+const results2 = findKey(test1Data, x => x.stars === 3);
+assertEqual(results2, "Akaleri");
 
-const results3 = findKey(test1Data, x => x.stars === 1)
-assertEqual(results3, "Blue Hill")
+const results3 = findKey(test1Data, x => x.stars === 1);
+assertEqual(results3, "Blue Hill");
 
-const results4 = findKey(test1Data, x => x.stars === 5)
-assertEqual(results4, undefined)
+const results4 = findKey(test1Data, x => x.stars === 5);
+assertEqual(results4, undefined);
