a5ee70142a6d1682e4e14a83e4bdf81d64455c0b
Tharsikan <thar.navaratnam@gmail.com>
2021-10-28 01:17:05 +0000

    remove accidental auto import

--- takeUntil.js
+++ takeUntil.js
@@ -1,40 +1,38 @@
-const { assert } = require("console");
-
 const logMessage = (isEqual, arr1, arr2) => {
   const message = isEqual ? `✅✅✅ Assertion Passed: ${arr1} === ${arr2}` : `❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`;
   console.log(message);
 };
 
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   return arr1.length !== arr2.length ? false : arr1.every((elem, i) => elem === arr2[i]);
 };
 
-const assertArraysEqual = function (arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   const isEqual = eqArrays(arr1, arr2);
   logMessage(isEqual, arr1, arr2);
 };
 
-const takeUntil = function (array, callback) {
+const takeUntil = function(array, callback) {
   const takenArr = [];
   for (const item of array) {
     if (!callback(item)) {
       takenArr.push(item);
     } else {
-      return takenArr
+      return takenArr;
     }
   }
-  return takenArr
-}
+  return takenArr;
+};
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-assertArraysEqual(results1, [1, 2, 5, 7, 2])
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood'])
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
 
 const data3 = ["to", "building", "lasagna", "heart", "not", "give"];
 const results3 = takeUntil(data3, x => x.length === 3);
-assertArraysEqual(results3, ["to", "building", "lasagna", "heart"])
+assertArraysEqual(results3, ["to", "building", "lasagna", "heart"]);
 
