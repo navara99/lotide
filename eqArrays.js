a3813a822063eb29b54ded46f48d5b6cb98d0b39
Tharsikan <thar.navaratnam@gmail.com>
2021-10-29 20:48:04 +0000

    Add more test cases

--- eqArrays.js
+++ eqArrays.js
@@ -30,6 +30,7 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4,[3,[3]]]], [[2, 3], [4,[3,[3]]]]), true);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
 assertEqual(eqArrays([[1, 2, 3, [2, [3, [4, [4, 5, [5]]]]]]], [[1, 2, 3, [2, [3, [4, [4, 5, [5]]]]]]]), true);
