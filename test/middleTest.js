803f81d9f8abbef326681dcac81a7ef07132f417
Tharsikan <thar.navaratnam@gmail.com>
2021-11-01 18:53:38 +0000

    test with mocha chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,30 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+const { expect } = require("chai");
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
+describe("#middle", () => {
+
+  it("should return middle element for even length array", () => {
+    const result = middle([1, 2, 3, 4]);
+    expect(result).to.deep.equal([2, 3]);
+  });
+
+  it("should return middle element for odd length array", () => {
+    const result = middle([1, 2, 3]);
+    expect(result).to.deep.equal([2]);
+  });
+
+  it("should return an empty array if the input is an array with length 1", () => {
+    const result = middle([1]);
+    expect(result).to.deep.equal([]);
+  })
+
+  it("should return an empty array if input array length is 2", () => {
+    const result = middle([1, 2]);
+    expect(result).to.deep.equal([]);
+  })
+
+  it("should return null if the input is not an array", () => {
+    const result = middle(12);
+    expect(result).to.equal(null);
+  })
+})
