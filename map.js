536820290096a84485ff8ebf525d2b3e6250da60
Tharsikan <thar.navaratnam@gmail.com>
2021-11-01 19:36:17 +0000

    refactor

--- map.js
+++ map.js
@@ -1,18 +1,6 @@
-const logMessage = (isEqual, arr1, arr2) => {
-  const message = isEqual ? `✅✅✅ Assertion Passed: ${arr1} === ${arr2}` : `❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`;
-  console.log(message);
-};
+const assertArraysEqual = require("./assertArraysEqual");
 
-const eqArrays = function (arr1, arr2) {
-  return arr1.length !== arr2.length ? false : arr1.every((elem, i) => elem === arr2[i]);
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  const isEqual = eqArrays(arr1, arr2);
-  logMessage(isEqual, arr1, arr2);
-};
-
-const map = function (array, callback) {
+const map = function(array, callback) {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
