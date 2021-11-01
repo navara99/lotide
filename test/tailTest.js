4f7f506d2d94e9d91d7ddcd6c6fd52b96666beec
Tharsikan <thar.navaratnam@gmail.com>
2021-11-01 19:37:38 +0000

    eslint

--- test/tailTest.js
+++ test/tailTest.js
@@ -4,9 +4,9 @@ const { expect } = require("chai");
 describe("#tail", () => {
   it("should return '['Lighthouse', 'Labs']' for valid input '['Hello', 'Lighthouse', 'Labs']'", () => {
     expect(tail(["Hello", "Lighthouse", "Labs"])).to.deep.equal(["Lighthouse", "Labs"]);
-  })
+  });
 
   it("should return null if input is not an array", () => {
     expect(tail(12)).to.equal(null);
-  })
+  });
 });
