46fff566a69e6f550e02c12049e5c62beacc2db1
Tharsikan <thar.navaratnam@gmail.com>
2021-11-01 22:45:02 +0000

    mocha chai tests

---
+++ test/flattenTest.js
@@ -0,0 +1,25 @@
+const flatten = require("../flatten");
+const { expect } = require("chai");
+
+describe("#flatten", () => {
+  it("should flatten nested arrays - [2, [3, 4, [5, 8]], 5, ['a']]", () => {
+    const result = flatten([2, [3, 4, [5, 8]], 5, ["a"]]);
+    expect(result).to.deep.equal([2, 3, 4, 5, 8, 5, "a"]);
+  });
+
+  it("should return empty array if input is an empty array", () => {
+    const result = flatten([]);
+    expect(result).to.deep.equal([]);
+  });
+
+  it("should return [3] if input is [3]", ()=> {
+    const result = flatten([3]);
+    expect(result).to.deep.equal([3]);
+  });
+
+  it("should return [3] if input is [3]", ()=> {
+    const result = flatten([3]);
+    expect(result).to.deep.equal([3]);
+  });
+
+});
