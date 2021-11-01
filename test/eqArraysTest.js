9c1cb64e5024542b7197214b4ac97c64398e78bb
Tharsikan <thar.navaratnam@gmail.com>
2021-11-01 22:35:50 +0000

    mocha chai tests

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,20 +1,25 @@
-const assertEqual = require("../assertEqual");
 const eqArrays = require("../eqArrays");
+const { expect } = require("chai");
 
-// Tests
-
-assertEqual(eqArrays([[2, 3, [2, 3, [2, 3]]], [4]], [[2, 3, [2, 3, [2, 3]]], [4]]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-assertEqual(eqArrays([[2, 3], [4,[3,[3]]]], [[2, 3], [4,[3,[3]]]]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
-assertEqual(eqArrays([[1, 2, 3, [2, [3, [4, [4, 5, [5]]]]]]], [[1, 2, 3, [2, [3, [4, [4, 5, [5]]]]]]]), true);
-assertEqual(eqArrays([[2, 3, 7, 8], [4]], [[2, 3, 5, [2, [2, [2, [2]]]]], 4]), false);
-assertEqual(eqArrays([[[[[[[[[[[1]]]]]]]]]]], [[1]]), false);
-assertEqual(eqArrays([[[[[[[[[[[1]]]]]]]]]], [[5]]], [[[[[[[[[[[1]]]]]]]]]], [[5]]]), true);
-assertEqual(eqArrays([[[[[[[[[[[1]]]]]]]]]], [[5]]], [[[[[[[[[[[1]]]]]]]]]], [[5, 7]]]), false);
-assertEqual(eqArrays([[[[[[[[[[[1, 2]]]], [1]]]]]]], [[5]]], [[[[[[[[[[[1, 2]]]], [1]]]]]]], [[5]]]), true);
+describe("#eqArrays", () => {
+  it("should return true when arrays are [[2, 3, [2, 3, [2, 3]]], [4]]", () => {
+    const result = eqArrays([[2, 3, [2, 3, [2, 3]]], [4]], [[2, 3, [2, 3, [2, 3]]], [4]]);
+    expect(result).to.equal(true);
+  });
+  it("should return true when nested arrays equal - [[[[[[[[[[[1, 2]]]], [1]]]]]]], [[5]]], [[[[[[[[[[[1, 2]]]], [1]]]]]]], [[5]]]", () => {
+    const result = eqArrays([[[[[[[[[[[1, 2]]]], [1]]]]]]], [[5]]], [[[[[[[[[[[1, 2]]]], [1]]]]]]], [[5]]]);
+    expect(result).to.equal(true);
+  });
+  it("should return false when nested arrays are not equal", () => {
+    const result = eqArrays([[2, 3, 7, 8], [4]], [[2, 3, 5, [2, [2, [2, [2]]]]], 4]);
+    expect(result).to.equal(false);
+  });
+  it("should return true when comparing equal arrays - '['1', '2', '3']' to ['1', '2', '3']",()=> {
+    const result = eqArrays(['1', '2', '3'], ['1', '2', '3']);
+    expect(result).to.equal(true);
+  });
+  it("should return false for non equal arrays - '['1', '2', '7']' to ['1', '2', '3']",()=> {
+    const result = eqArrays(['1', '2', '7'], ['1', '2', '3']);
+    expect(result).to.equal(false);
+  });
+});
