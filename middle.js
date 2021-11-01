a29362f0ddcbe909b6537f7bdd6797b2d2fc7a58
Tharsikan <thar.navaratnam@gmail.com>
2021-11-01 18:55:05 +0000

    handle non array edge case

--- middle.js
+++ middle.js
@@ -3,6 +3,7 @@ const getMiddleIndex = (arr) => Math.floor((arr.length - 1) / 2);
 const getMiddleArr = (middleIndex, arr) => arr.length % 2 !== 0 ? [arr[middleIndex]] : [arr[middleIndex], arr[middleIndex + 1]];
 
 const middle = function(arr) {
+  if (!Array.isArray(arr)) return null;
   if (arr.length === 1 || arr.length === 2) return [];
   const middleIndex = getMiddleIndex(arr);
   const middleArr = getMiddleArr(middleIndex, arr);
