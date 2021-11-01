152cbc2c9cb93ecf87b2b5e14d9bd48ddb2b101f
Tharsikan <thar.navaratnam@gmail.com>
2021-11-01 23:17:55 +0000

    change to deep equals test

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -6,25 +6,25 @@ describe("#eqObjects", () => {
     const a = { a: "1", b: "2" };
     const b = { b: "2", a: "1" };
     const result = eqObjects(a, b);
-    expect(result).to.equal(true);
+    expect(result).to.deep.equal(true);
   });
   it("should return true when nested objects are equal ", () => {
     const a = { a: { y: [0, 1, 2, 3, [1]], z: 1 }, b: 2 };
     const b = { a: { y: [0, 1, 2, 3, [1]], z: 1 }, b: 2 };
     const result = eqObjects(a,b);
-    expect(result).to.equal(true);
+    expect(result).to.deep.equal(true);
   });
   it("should return false when nested objects are not equal ", () => {
     const a = { a: { y: [0, 1, 2, 3, [1]], z: 1 }, b: 2 };
     const b = { a: { y: [0, 1, 2, 3, [3]], z: 1 }, b: 2 };
     const result = eqObjects(a,b);
-    expect(result).to.equal(false);
+    expect(result).to.deep.equal(false);
   });
   it("should return false when valid objects are not equal", () => {
     const a = { a: "1", b: "2" };
     const b = { b: "7", a: "1" };
     const result = eqObjects(a, b);
-    expect(result).to.equal(false);
+    expect(result).to.deep.equal(false);
   });
 
 });
