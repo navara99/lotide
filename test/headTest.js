d84a7d6f445b58229aaae963a7f66f66ef3fe0d6
Tharsikan <thar.navaratnam@gmail.com>
2021-11-01 18:14:52 +0000

    test with mocha chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,28 @@
-const assertEqual = require("../assertEqual");
 const head = require("../head");
+const { assert } = require("chai");
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head([]), undefined);
-assertEqual(head(["Javascript"]), "Javascript");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Python", "Ruby"]), "Python");
+describe("#head", () => {
+  it("returns first element for a valid array", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  })
+
+  it("returns first element for an array with 1 element", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns undefined for empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it("returns 'Hello' input is ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns 'Python' when input is ['Python', 'Ruby'])", () => {
+    assert.strictEqual(head(['Python', 'Ruby']), "Python");
+  })
+
+  it("returns null for a non array input", () => {
+    assert.strictEqual(head("12"), null);
+  });
+})
