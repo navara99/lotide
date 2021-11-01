9b1d2c07a125a127ceecfb1929b8cf46e3433556
Tharsikan <thar.navaratnam@gmail.com>
2021-11-01 23:15:22 +0000

    mocha chai tests

---
+++ test/countLettersTest.js
@@ -0,0 +1,17 @@
+const { expect } = require("chai");
+const countLetters = require("../countLetters");
+
+// assertEqual(countLetters("lighthouse in the house")["h"], 4);
+// assertEqual(countLetters("Joe waited for the train. The train was late.")["e"], 5);
+
+describe("#countLetters", () => {
+  it("should return 4 for countLetters('lighthouse in the house')['h']", () => {
+    const result = countLetters("lighthouse in the house")["h"];
+    expect(result).to.equal(4);
+  });
+
+  it("should return 5 for countLetters('Joe waited for the train. The train was late.')['e']", () => {
+    const result = countLetters("Joe waited for the train. The train was late.")["e"];
+    expect(result).to.equal(5);
+  });
+});
