ba631b7854a28ecdaa8373fdd7d19114fb81bcd4
Tharsikan <thar.navaratnam@gmail.com>
2021-11-01 23:05:46 +0000

    deleted comments

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,23 +1,6 @@
 const { expect } = require("chai");
 const eqObjects = require("../eqObjects");
 
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// assertEqual(eqObjects(ab, ba), true);
-
-// const abc = { a: "1", b: "2", c: "3" };
-// assertEqual(eqObjects(ab, abc), false);
-
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// assertEqual(eqObjects(cd, dc), true);
-
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertEqual(eqObjects(cd, cd2), false);
-
-// { a: { y: [0, 1, 2, 3, [1]], z: 1 }, b: 2 }, { a: { y: [0, 1, 2, 3, [1]], z: 1 }, b: 2 }), true
-
-
 describe("#eqObjects", () => {
   it("should return true when objects for equal valid objects", () => {
     const a = { a: "1", b: "2" };
