b7412722dd78e7cb7eb46fa3610e08ac57ba715d
Tharsikan <thar.navaratnam@gmail.com>
2021-11-02 00:57:54 +0000

    mocha chai test

---
+++ test/countOnlyTest.js
@@ -0,0 +1,21 @@
+const { expect } = require("chai");
+const countOnly = require("../countOnly");
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+describe("#countOnly", () => {
+  it("should return {Jason: 1, Fang: 2} for { Jason: true, Karima: true, Fang: true, Agouhanna: false }", () => {
+    const result = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+    expect(result).to.deep.equal({ Jason: 1, Fang: 2 });
+  });
+});
